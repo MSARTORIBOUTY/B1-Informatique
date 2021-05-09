19ee384a62216fbb88609bbfec17e0b036e7ecaf
MSARTORIBOUTY <marielaure.sartoribouty@ynov.com>
2021-05-09 13:43:02 +0200

    piscine goland final commit

--- piscine_go/QUÊTE 8/boolean/main.go
+++ piscine_go/QUÊTE 8/boolean/main.go
@@ -1,4 +1,4 @@
-//Boolean
+
 package main
 
 import (
@@ -32,40 +32,4 @@ func main() {
 
 }
 
-//Cyprien
-/*package main
 
-import (
-	"os"
-
-	"github.com/01-edu/z01"
-)
-
-func main() {
-	args := os.Args[1:]
-	if isEven(len(args)) == true {
-		printStr("EvenMsg")
-	} else {
-		printStr("OddMsg")
-	}
-}
-func isEven(nbr int) bool {
-	if nbr%2 == 0 {
-		return true
-	}
-	return false
-}
-
-func printStr(s string) {
-	var texte string
-
-	if s == "EvenMsg" {
-		texte = "I have an even number of arguments\n"
-	} else {
-		texte = "I have an odd number of arguments\n"
-	}
-
-	for _, c := range texte {
-		z01.PrintRune(rune(c))
-	}
-}*/
